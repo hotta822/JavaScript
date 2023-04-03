05275cd26c752230cbc99cbf092b8c32d003ff74
hotta822 <126322004+hotta822@users.noreply.github.com>
2023-04-03 23:00:07 +0900

    変数・定数

---
+++ 1-2/main.js
@@ -0,0 +1,15 @@
+/*
+チャレンジ問題
+エラー個所を特定して200、1000が出力されるように修正する
+const price = 100;
+console.log(price*2);
+
+price = 500;
+console.log(price*2);
+*/
+//constは再代入も不可のため宣言の仕方でエラーが出ている。
+let price = 100;
+console.log(price*2);
+
+price = 500;
+console.log(price*2);
