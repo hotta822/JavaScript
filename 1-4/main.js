4ce0ef44f55595a33969d77a8e7df78b6df99cf9
hotta822 <126322004+hotta822@users.noreply.github.com>
2023-04-03 23:08:02 +0900

    FizzBuzz!!

---
+++ 1-4/main.js
@@ -0,0 +1,21 @@
+/*
+理解度チェック
+出力の結果はconsole.logで表示
+3の倍数のとき、 Fizz! と出力します。
+5の倍数のとき、 Buzz! と出力します。
+3の倍数かつ5の倍数のとき、 FizzBuzz!! と出力します。(15の倍数、と考えるのはナシです。)
+3の倍数でも、5の倍数でもない場合は、その数を出力します。
+それを1〜100まで実施してください。
+*/
+
+for(let num=1;num<=100;++num){
+    if(num%5===0 && num%3===0){
+        console.log("FizzBuzz!!");
+    }else if(num%5===0){
+        console.log("Buzz!");
+    }else if(num%3===0){
+        console.log("Fizz");
+    }else{
+        console.log(num);
+    }
+}
