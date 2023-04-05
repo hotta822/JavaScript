69034fbb9dd8f63f2ce052e8ca1e12358828be38
hotta822 <126322004+hotta822@users.noreply.github.com>
2023-04-05 17:02:09 +0900

    配列、オブジェクト

---
+++ 1-6/main.js
@@ -0,0 +1,17 @@
+//問１
+let score = [10,15,20,25];
+console.log(score);
+
+console
+for(let i=0;i<score.length;i++){
+    if(score[i]%2===0){
+        console.log(score[i]+"は偶数です")
+    }
+}
+//問２
+let car = {
+    gass: 20.5,
+    num: 1234,
+}
+console.log("ガソリンは"+car.gass+"です");
+console.log("ナンバーは"+car.num+"です");
